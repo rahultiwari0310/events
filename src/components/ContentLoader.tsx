b1212a55f3002a8ed9b6fbc181829b0e37f11b3a
rahultiwari0310 <rahultiwari0310@gmail.com>
2023-02-12 00:39:18 +0530

    chore: Add gh-pages for deployment

--- src/components/ContentLoader.tsx
+++ src/components/ContentLoader.tsx
@@ -1,6 +1,4 @@
-import { ReactNode } from "react";
 import { Spinner } from "./Spinner";
-import styled from 'styled-components';
 
 export const ContentLoader = ({ isLoading, error, children }: { isLoading?: boolean; error?: string | null; children: JSX.Element }) => {
     if (isLoading) {
