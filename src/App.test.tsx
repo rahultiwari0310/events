3a4878223a19df70d080e869c88fc7ee3d53cd7f
rahultiwari0310 <rahultiwari0310@gmail.com>
2023-02-12 13:58:56 +0530

    fix: Wrote end to end test cases

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,73 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { act, render, screen, waitFor } from '@testing-library/react';
 import App from './App';
+import mockEvents from './stubs/events'
 
-test('renders learn react link', () => {
-  render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-});
+jest.mock("./hooks/useFetchEvents", () => ({
+  useFetchEvents: () => ({
+    eventsState: {
+      data: mockEvents,
+      isLoading: false,
+      error: null
+    }
+  })
+}))
+
+describe("Events App", () => {
+
+  it("Should render all event names", () => {
+    render(<App />)
+    for (let i = 0; i < mockEvents.length; i++) {
+      const it = mockEvents[i];
+      expect(screen.getByText(it.event_name)).toBeInTheDocument()
+    }
+  })
+
+  it("Should move an event to selected section on button click", async () => {
+    const { container } = render(<App />);
+    const eventId = mockEvents[0].id;
+    const selectedEventsContainer = container.querySelectorAll('section')[1];
+    expect(selectedEventsContainer.querySelectorAll(`[data-testid=event-${eventId}]`).length).toBe(0);
+    act(() => {
+      screen.getByTestId(`event-${eventId}`).getElementsByTagName("button")[0].click();
+    })
+    expect(selectedEventsContainer.querySelectorAll(`[data-testid=event-${eventId}]`).length).toBe(1);
+  })
+
+  it("Should not allow selection for an overlapping event", async () => {
+    const { container } = render(<App />);
+    const eventId = mockEvents[0].id;
+    const selectedEventsContainer = container.querySelectorAll('section')[1];
+    expect(selectedEventsContainer.querySelectorAll(`[data-testid=event-${eventId}]`).length).toBe(0);
+    act(() => {
+      screen.getByTestId(`event-${eventId}`).getElementsByTagName("button")[0].click();
+    })
+    const overlappingEventId = `event-${mockEvents[1].id}`
+    act(() => {
+      screen.getByTestId(overlappingEventId).getElementsByTagName("button")[0].click();
+    })
+    expect(selectedEventsContainer.querySelectorAll(`[data-testid=${overlappingEventId}]`).length).toBe(0);
+  })
+
+  it("Should not allow selection when 3 events selected", async () => {
+    const { container } = render(<App />);
+    const selectedEventsContainer = container.querySelectorAll('section')[1];
+    act(() => {
+      screen.getByTestId(`event-${mockEvents[0].id}`).getElementsByTagName("button")[0].click();
+    })
+    act(() => {
+      screen.getByTestId(`event-${mockEvents[2].id}`).getElementsByTagName("button")[0].click();
+    })
+    act(() => {
+      screen.getByTestId(`event-${mockEvents[4].id}`).getElementsByTagName("button")[0].click();
+    })
+    act(() => {
+      screen.getByTestId(`event-${mockEvents[5].id}`).getElementsByTagName("button")[0].click();
+    })
+    expect(selectedEventsContainer.querySelectorAll(`[data-testid=event-${mockEvents[0].id}]`).length).toBe(1);
+    expect(selectedEventsContainer.querySelectorAll(`[data-testid=event-${mockEvents[2].id}]`).length).toBe(1);
+    expect(selectedEventsContainer.querySelectorAll(`[data-testid=event-${mockEvents[4].id}]`).length).toBe(1);
+    expect(selectedEventsContainer.querySelectorAll(`[data-testid=event-${mockEvents[5].id}]`).length).toBe(0);
+  })
+
+
+})
--- src/components/tests/EventItem.test.tsx
+++ src/components/tests/EventItem.test.tsx
@@ -1,13 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import { EventItem } from '../EventItem';
+import mockEvents from '../../stubs/events';
 
-const mockEvent = {
-    "id": 1,
-    "event_name": "Butterfly 100M",
-    "event_category": "Swimming",
-    "start_time": "2022-12-17 13:00:00",
-    "end_time": "2022-12-17 14:00:00"
-}
+const mockEvent = mockEvents[0];
 
 describe('Event Item component', () => {
     it('renders event details correctly', () => {
