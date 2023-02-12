615b71d72f7d1ca85c49462df82582fa0cb48e41
rahultiwari0310 <rahultiwari0310@gmail.com>
2023-02-12 12:28:09 +0530

    feat: test

--- src/utils/checkTimeOverlap.ts
+++ src/utils/checkTimeOverlap.ts
@@ -1,22 +1,21 @@
 import { SportingEvent } from "../hooks/useFetchEvents";
-import dayjs from 'dayjs';
-import isBetween from 'dayjs/plugin/isBetween';
+import dayjs from "dayjs";
+import isBetween from "dayjs/plugin/isBetween";
 
-dayjs.extend(isBetween)
-type DateRange = Pick<SportingEvent, 'end_time' |'start_time'>;
+dayjs.extend(isBetween);
+type DateRange = Pick<SportingEvent, "end_time" | "start_time">;
 /**
- * 
- * @param eventA 
- * @param eventB 
+ *
+ * @param eventA
+ * @param eventB
  * @returns true if duration overlaps
  */
-export const checkTimeOverlaps = (
-  eventA: DateRange,
-  eventB: DateRange
-) => {
-
+export const checkTimeOverlaps = (eventA: DateRange, eventB: DateRange) => {
   // If events begin and end at same time, fail fast.
-  if(eventA.start_time === eventB.start_time && eventA.end_time === eventB.end_time) {
+  if (
+    eventA.start_time === eventB.start_time &&
+    eventA.end_time === eventB.end_time
+  ) {
     return true;
   }
 
@@ -25,5 +24,10 @@ export const checkTimeOverlaps = (
   const eventAEndTime = dayjs(eventA.end_time);
   const eventBEndTime = dayjs(eventB.end_time);
 
-  return eventAStartTime.isBetween(eventBStartTime, eventBEndTime) || eventAEndTime.isBetween(eventBStartTime, eventBEndTime) || eventBStartTime.isBetween(eventAStartTime, eventAEndTime) || eventBEndTime.isBetween(eventAStartTime, eventAEndTime);
+  return (
+    eventAStartTime.isBetween(eventBStartTime, eventBEndTime) ||
+    eventAEndTime.isBetween(eventBStartTime, eventBEndTime) ||
+    eventBStartTime.isBetween(eventAStartTime, eventAEndTime) ||
+    eventBEndTime.isBetween(eventAStartTime, eventAEndTime)
+  );
 };
